711755b76058cf4f1f678361a923832e1725e6dd
zichengsaber <zzczzc.1221@gmail.com>
2021-10-28 16:40:40 +0800

    还是觉得有点难搞

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 import torch 
 from model.FasterRCNNVGG import FasterRCNNVGG16
 from data.Vocdata import VOCDetectionDataset
-
+import os
+from trainer import FasterRCNNTrainer
+os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 dataset=VOCDetectionDataset("/home/ZhangZicheng/ObjectionDetection/VOCdevkit/VOC2007")
-img,_=dataset[0]
 
-model=FasterRCNNVGG16()
-model=model.cuda()
+faster_rcnn=FasterRCNNVGG16()
+trainer=FasterRCNNTrainer(faster_rcnn).cuda()
 
-roi_cls_locs,roi_scores,rois,roi_indices=model(img[None].cuda())
-print(roi_cls_locs.size())
-print(roi_scores.size())
-print(rois.size())
-print(roi_indices.size())
+PATH="../pretrained_model/fasterrcnn_12211511_0.701052458187_torchvision_pretrain.pth"
 
+# load the model
 
+trainer.load(PATH)
+print("load the pretrained model")
 
 
